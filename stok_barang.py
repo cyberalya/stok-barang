ee6ceede106308a292affbf11b317f2e4a24ee9f
gf'hyunjin <alyahafizhah03@gmail.com>
2025-06-26 20:04:07 +0700

    Update stok_barang.py

--- stok_barang.py
+++ stok_barang.py
@@ -98,5 +98,3 @@ def generate_receipt(nama_toko, alamat, no_hp, nama_barang, jumlah, harga_satuan
     </body>
     </html>
     """
-
-# (Sisa kode tetap, tidak perlu diubah ulang karena instruksi hanya menyangkut struk print otomatis tab baru)
